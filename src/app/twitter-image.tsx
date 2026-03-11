42796d784fc816f933f03149eb6fe7200086aa4f
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2026-03-11 20:18:08 -0300

    a

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,1 +1,15 @@
-export { default, runtime, alt, size, contentType } from "./opengraph-image";
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt =
+  "Jiku - Plataforma de turnos, CRM y pagos para negocios de servicios en Argentina";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export { default } from "./opengraph-image";
